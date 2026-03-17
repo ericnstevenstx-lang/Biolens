17074b9aef21ce41468851b619fac8f3dae39bcb
ericnstevenstx-lang <ericnstevenstx@gmail.com>
2026-03-17 02:32:37 -0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: ['neo4j-driver'],
-  // Exclude the legacy CRA src/ directory from Next.js compilation
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/node_modules', '**/src/**'],
-    }
-    return config
-  },
-  // Only compile files in app/ directory
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
